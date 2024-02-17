3069238e918396a9ddb0a3dfe9c48e089e477804
Kaloyan Ivanov <KKIvanov22@codingburgas.bg>
2024-02-17 21:00:57 +0200

    Implement question system

--- functions.cpp
+++ functions.cpp
@@ -3,24 +3,39 @@
 #include <chrono>
 #include <thread>
 
+
+Rectangle answear1 = { 150, 325, 200, 50 };
+Rectangle answear2 = { 150, 400, 200, 50 };
+Rectangle answear3 = { 150, 475, 200, 50 };
+Rectangle answear4 = { 150, 550, 200, 50 };
+
+int laserspeed = 7;
+int movementSpeed = 4;
+int asteroidspeed = 2; 
+
+bool IsQuestionAnsweared = 0;
+int AsteroidSpawnTime = 4;
+
+int points = 0;
+
 void movement(int& sposx, int& sposy) {
     if (IsKeyDown(KEY_RIGHT)) {
-        sposx += 4;
+        sposx += movementSpeed;
     }
     if (IsKeyDown(KEY_LEFT)) {
-        sposx -= 4;
+        sposx -= movementSpeed;
     }
     if (IsKeyDown(KEY_UP)) {
-        sposy -= 4;
+        sposy -= movementSpeed;
     }
     if (IsKeyDown(KEY_DOWN)) {
-        sposy += 4;
+        sposy += movementSpeed;
     }
 }
 
 void updateLaser(Laser& laser) {
     if (laser.active) {
-        laser.y -= 7;
+        laser.y -= laserspeed;
 
         if (laser.y <= 0) {
             laser.active = false;
@@ -57,21 +72,71 @@ void drawLaserParticles(const vector<LaserParticle>& particles) {
 
 void updateAsteroid(Asteroid& asteroid, Laser& laser, vector<Particle>& particles) {
     if (asteroid.active) {
-        asteroid.y += 2;
+        asteroid.y += asteroidspeed;
 
         if (CheckCollisionRecs({ (float)laser.x, (float)laser.y, 5, 20 }, { (float)asteroid.x - asteroid.size / 2, (float)asteroid.y - asteroid.size / 2, (float)asteroid.size, (float)asteroid.size })) {
-            asteroid.active = false;
-            laser.active = false;
+            
+            DrawRectangle(100, 250, 600, 400, GRAY);
 
-            for (int i = 0; i < 30; ++i) {
-                Particle particle;
-                particle.position = { static_cast<float>(asteroid.x), static_cast<float>(asteroid.y) };
-                particle.velocity = { static_cast<float>(GetRandomValue(-5, 5)), static_cast<float>(GetRandomValue(-5, 5)) };
-                particle.color = GRAY;
-                particle.radius = GetRandomValue(1, 3);
-                particle.active = true;
-                particles.push_back(particle);
+            laserspeed = 0;
+            movementSpeed = 0;
+            asteroidspeed = 0;
+            AsteroidSpawnTime = 999999;
+
+            int QuestionNumber = GetRandomValue(1, 1);
+
+            if (QuestionNumber == 1)
+            {
+                DrawText("TEST", 200, 275, 15, BLACK);
+                DrawRectangleRec(answear1, WHITE);
+                DrawText("TEST", 160, 335, 15, BLACK);
+                DrawRectangleRec(answear2, WHITE);
+                DrawText("TEST", 160, 410, 15, BLACK);
+                DrawRectangleRec(answear3, WHITE);
+                DrawText("TEST", 160, 485, 15, BLACK);
+                DrawRectangleRec(answear4, WHITE);
+                DrawText("TEST", 160, 560, 15, BLACK);
+                if ((IsMouseButtonPressed(MOUSE_BUTTON_LEFT) || IsMouseButtonPressed(MOUSE_BUTTON_RIGHT)) && CheckCollisionPointRec(GetMousePosition(), answear1))
+                {
+                    IsQuestionAnsweared = 0;
+                }
+                if ((IsMouseButtonPressed(MOUSE_BUTTON_LEFT) || IsMouseButtonPressed(MOUSE_BUTTON_RIGHT)) && CheckCollisionPointRec(GetMousePosition(), answear2))
+                {
+                    IsQuestionAnsweared = 1;
+                    points++;
+                }
+                if ((IsMouseButtonPressed(MOUSE_BUTTON_LEFT) || IsMouseButtonPressed(MOUSE_BUTTON_RIGHT)) && CheckCollisionPointRec(GetMousePosition(), answear3))
+                {
+                    IsQuestionAnsweared = 0;
+                }
+                if ((IsMouseButtonPressed(MOUSE_BUTTON_LEFT) || IsMouseButtonPressed(MOUSE_BUTTON_RIGHT)) && CheckCollisionPointRec(GetMousePosition(), answear4))
+                {
+                    IsQuestionAnsweared = 0;
+                }
             }
+            if (IsQuestionAnsweared == 1)
+            {
+                asteroid.active = false;
+                laser.active = false;
+
+                for (int i = 0; i < 30; ++i) {
+                    Particle particle;
+                    particle.position = { static_cast<float>(asteroid.x), static_cast<float>(asteroid.y) };
+                    particle.velocity = { static_cast<float>(GetRandomValue(-5, 5)), static_cast<float>(GetRandomValue(-5, 5)) };
+                    particle.color = GRAY;
+                    particle.radius = GetRandomValue(1, 3);
+                    particle.active = true;
+                    particles.push_back(particle);
+                }
+
+                IsQuestionAnsweared = 0;
+                laserspeed = 7;
+                movementSpeed = 4;
+                asteroidspeed = 2;
+                AsteroidSpawnTime = 4;
+
+            }
+            
         }
 
         if (asteroid.y >= 800) {
@@ -126,7 +191,7 @@ void initgame()
         auto start = chrono::steady_clock::now();
         BeginDrawing();
         ClearBackground(BLACK);
-
+        
         DrawRectangle(sposx, sposy, 50, 50, YELLOW);
         movement(sposx, sposy);
 
@@ -152,9 +217,9 @@ void initgame()
         updateLaserParticles(laserParticles);
         drawLaserParticles(laserParticles);
         chrono::steady_clock::time_point currentTime = chrono::steady_clock::now();
-        if (chrono::duration_cast<chrono::seconds>(currentTime - lastSpawnTime).count() >= 4) {
+        if (chrono::duration_cast<chrono::seconds>(currentTime - lastSpawnTime).count() >= AsteroidSpawnTime) {
             Asteroid asteroid;
-            asteroid.x = GetRandomValue(20, 780);
+            asteroid.x = GetRandomValue(50, 750);
             asteroid.y = -20;
             asteroid.size = GetRandomValue(30, 200);
             asteroid.active = true;
