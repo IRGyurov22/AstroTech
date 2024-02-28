a2531a264d5458abc9c1d13636220103b1d3b962
asivanov22 <asivanov22@codingburgas.bg>
2024-02-28 11:35:25 +0200

    Add the 'control' option

--- main.cpp
+++ main.cpp
@@ -11,8 +11,8 @@ int main()
     int menuHeight = 800;
     InitWindow(menuWidth, menuHeight, "Menu");
 
-    Rectangle start = { 300, 300, 200, 100 };
-    Rectangle settings = { 300, 400, 200, 100 };
+    Rectangle start = { 300, 325, 200, 100 };
+    Rectangle controls = { 300, 400, 200, 100 };
     Rectangle exit = { 300, 500, 200, 100 };   
     
     Texture2D background = LoadTexture("resources/photos/background.png");
@@ -25,12 +25,13 @@ int main()
         BeginDrawing();
         DrawTexture(background, 0, 0, WHITE);
         DrawRectangleRec(start, BLANK);
-        DrawRectangleRec(settings, BLANK);
+        DrawRectangleRec(controls, BLANK);
         DrawRectangleRec(exit, BLANK);
         DrawTextEx(font, "Space Cadet", { 245, 100 }, 65, 1, WHITE);
         DrawTextEx(font, "by AstroTech", { 302, 170 }, 40, 1, WHITE);
-        DrawTextEx(font, "START", { 320, 370 }, 65, 1, WHITE);
-        DrawTextEx(font, "EXIT", { 345, 490 }, 65, 1, WHITE);
+        DrawTextEx(font, "START", { 320, 350 }, 55, 1, WHITE);
+        DrawTextEx(font, "CONTROLS", { 275, 460 }, 55, 1, WHITE);
+        DrawTextEx(font, "EXIT", { 343, 570 }, 55, 1, WHITE);
         if ((IsMouseButtonPressed(MOUSE_BUTTON_LEFT) || IsMouseButtonPressed(MOUSE_BUTTON_RIGHT)) && CheckCollisionPointRec(GetMousePosition(), start))
         {
             startgame = 1;
